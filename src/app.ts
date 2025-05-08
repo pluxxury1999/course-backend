40074382204a60401545f4d86f6b495e75112c41
pluxxury <pedobirandfriends@gmail.com>
2025-05-08 21:49:02 +0300

    adding cors package

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import cors from "cors";
 import express from "express";
 import faqRouter from "./routes/faq";
 import itemsRouter from "./routes/item";
 import userQuestionsRouter from "./routes/userQuestions";
 
 const app = express();
+
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+app.use(cors({ origin: FRONTEND_URL }));
+
 app.use(express.json());
 app.use("/items", itemsRouter);
 app.use("/faq", faqRouter);
